b2b858322a8555fbedb05a32c5290da5cd2e61ef
Rodrigo Melo <nalajejazz@gmail.com>
2022-06-15 00:34:59 -0300

    Ajustes na colisão

--- obj.js
+++ obj.js
@@ -3,29 +3,35 @@
 export const canvas = document.getElementById("Canvas");
 export const ctx = canvas.getContext("2d");
 export default class Obj {
-  constructor(x, y, w, h, spd, c) {
+  constructor(x, y, w, h, spd) {
     this.x = x
     this.y = y
     this.w = w
     this.h = h
-    this.c = c
+   
     this.spd = spd
-    this.collideBolean = false
+    
 
 
   }
-  draw(){
-    ctx.fillStyle = this.c;
+  draw(c){
+    ctx.save()
+    this.c=c
+    ctx.fillStyle =c;
     ctx.fillRect(this.x, this.y, this.w,this.h);
+    ctx.restore()
   }
 
    collide(hitX,hitY,hitW,hitH){
+    this.collideBolean = false;
     this.hitX=hitX;
     this.hitY=hitY;
     this.hitW=hitW;
     this.hitH=hitH;
 if(this.x<=this.hitX+this.hitW&&this.x+this.w>=this.hitX&&this.y+this.h>=this.hitY&&this.y<=this.hitY+this.hitH)
-{this.collideBolean=true}else{this.collideBolean=false}}; 
+{this.collideBolean=true}else{this.collideBolean=false}
+
+}; 
 
 };
 
--- script.js
+++ script.js
@@ -4,21 +4,19 @@ import Obj from './obj.js'
 import {canvas} from './obj.js'
 import {ctx} from './obj.js'
 
-var moveR=false,moveL=false,moveU=false,moveD=false,hit=false;
+let moveR=false,moveL=false,moveU=false,moveD=false,hit=false;
 canvas.width=screen.width;
 canvas.height=screen.height;
 canvas.style.backgroundColor="black";
 
-var o=[]
+let i = new Obj(800,350,64,64,3);
+let o=[];
 
 for(var j = 0 ; j<3 ; j++ ){
 
   o[j]= new Obj(400,250,64,64,0,"blue")
  
-}
-
-//let o = new Obj(400,250,128,128,0,"blue")
-let i = new Obj(600,350,64,64,3,"red")
+};
 
 window.addEventListener("keyup",()=>{
     moveL=false;
@@ -30,7 +28,7 @@ window.addEventListener("keyup",()=>{
 window.addEventListener("keydown",function(event){
 
         let k= event.key;
-    
+ 
         if (k == "d" ){
             moveR=true;
                 
@@ -42,37 +40,40 @@ window.addEventListener("keydown",function(event){
         }else if (k=="s"){
           moveD=true;
         }
-            
+    
     },false);
 
 function game (){
 requestAnimationFrame(game,canvas);
-
+ctx.clearRect(0,0,canvas.width,canvas.height);
+ 
 if(moveR){i.x+=i.spd}
 if(moveL){i.x-=i.spd}
 if(moveU){i.y-=i.spd}
 if(moveD){i.y+=i.spd}
-  
-  ctx.clearRect(0,0,canvas.width,canvas.height);
-  ctx.save();
 
 
-  i.draw()
-  
+
+  i.draw("red")
+
+ 
+//console.log(i.collideBolean)
+ 
   for(var j = 0 ; j<3 ; j++ ){
 
     o[j].draw()
-    o[j].x=128*j
+    o[j].x=128*j+200
     o[j].collide(i.x,i.y,i.w,i.h)
-    //i.collide(o[j].x,o[j].y,o[j].w,o[j].h)
+    
   }
 
-  ctx.font = "60px Courier New";
+  ctx.font = "30px Courier New";
   ctx.fillStyle = "white";
   ctx.textAlign = "center";
-  ctx.fillText(`collidebollean=${o[2].collideBolean}`, canvas.width/2, canvas.height-30);
-
-  ctx.restore()
+  ctx.fillText(`
+  [0]= ${o[0].collideBolean}
+   [1]= ${o[1].collideBolean}
+    [2]= ${o[2].collideBolean}`, canvas.width/2, canvas.height-30);
 
 };
 game();
