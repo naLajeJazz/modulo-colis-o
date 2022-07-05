735adec00d8be2b31c78a24610e6349709afce9e
Rodrigo Melo <nalajejazz@gmail.com>
2022-07-05 00:50:25 -0300

    Done tiles transition

--- script.js
+++ script.js
@@ -18,17 +18,19 @@ let player = new Obj(canvas.width/2+32,canvas.height/2+32,64,64,3),
     moveD=false,
     move=false;
 
-let collisionUp= new Obj(0,0,canvas.width,32), collisionLeft= new Obj(0,0,32,canvas.height),
-    collisionRight= new Obj(canvas.width-32,0,32,canvas.height),
-    collisionDown= new Obj(0,608,canvas.width,32);
+
 
 let tileDungeon= new Obj(canvas.width/2-240,canvas.height/2-240,480,480),
     d1=new Obj(canvas.width/2-64,canvas.height/2-246,128,8),
     d3=new Obj(canvas.width/2-64,canvas.height/2+240,128,8),
     d4=new Obj(canvas.width/2-248,canvas.height/2-64,8,128),
-    d2=new Obj(canvas.width/2+240,canvas.height/2-64,8,128);
+    d2=new Obj(canvas.width/2+240,canvas.height/2-64,8,128)
+   
+    ;
 
-
+    let collisionUp= new Obj(0,0,canvas.width,32), collisionLeft= new Obj(0,0,32,canvas.height),
+    collisionRight= new Obj(canvas.width-32,0,32,canvas.height),
+    collisionDown= new Obj(0,608,canvas.width,32);
 
 ///anima Sprite
 let xIndex=0
@@ -36,8 +38,9 @@ let yIndex=0
 let animaSpd=8//tem que ser multiplos de 2
 setInterval(()=>xIndex+=64,1000/animaSpd);//a cada segundo pula 64 px na imagem
 setInterval(()=>xIndex=0,4000/animaSpd);//quando chegar na ultima imagem volta pra primeira
-let xTiles=960
-let yTiles=960
+let xTiles=0
+let yTiles=0
+let tileId=null
 
 
 let playerSprite = document.createElement("IMG");
@@ -48,7 +51,7 @@ let bck3 = document.createElement("IMG");
 bck3.setAttribute("src","./assets/tile_3.png");
 
 
-
+let c=0
 
 window.addEventListener("keyup",()=>{
   moveL=false;
@@ -77,12 +80,25 @@ window.addEventListener("keydown",function(event){
         }else if (k=="s"){
           moveD=true;
           move=true
+        }else if (k=="l"){
+          xTiles+=480
+        }else if (k=="k"){
+          xTiles-=480
+        }else if (k=="i"){
+          yTiles-=480
+        }else if (k=="m"){
+          yTiles+=480
         }
     },false);
 
+    
    
-  
-   
+
+
+
+
+
+    
 function game (){
 requestAnimationFrame(game,canvas);
 ctx.clearRect(0,0,canvas.width,canvas.height);
@@ -91,22 +107,44 @@ ctx.clearRect(0,0,canvas.width,canvas.height);
 
 tileDungeon.SpriteTiles(bck2,xTiles,yTiles);
 
+if(xTiles==0 &yTiles==0){
+  tileId="ts7"
+  collisionUp.x=tileDungeon.x
+  collisionUp.y=tileDungeon.y+16
+  collisionUp.w=tileDungeon.w
+  collisionDown.x=tileDungeon.x
+  collisionDown.y=tileDungeon.y+456
+  collisionDown.w=tileDungeon.w
+}else if(xTiles==480 &yTiles==0){
+  collisionUp.y=300
+  tileId="ts23"
+}
+
 
 //Transição do sprite no tile sides
 if(player.y+64<d1.y){
-  player.y=d3.y-32;
+  yTiles-=480
+  player.y=d3.y-32; 
+ 
 }else if(player.y>d3.y-16){
+
+  yTiles+=480
   player.y=d1.y-16
 }
  if (player.x>d2.x-16){
+
+  xTiles+=480
   player.x=d4.x-8;
 }else if (player.x+32<d4.x){
+  
+  xTiles-=480
   player.x=d2.x-32
 };
 //d1.draw("red");
 //d3.draw('green');
 //d2.draw("blue");
-//d4.draw("orange")
+//d4.draw("orange");
+
 
 
 ///anima Sprite
@@ -122,14 +160,18 @@ if (moveD){
 }
 else{player.SpriteAnime(playerSprite,xIndex,yIndex+256);}
 
+//player.draw("red")
 
-/*
+
 ///check collision
 collisionUp.collide(player.x,player.y,player.w,player.h)
 collisionLeft.collide(player.x,player.y,player.w,player.h)
 collisionRight.collide(player.x,player.y,player.w,player.h)
 collisionDown.collide(player.x,player.y,player.w,player.h)
-*/
+
+
+
+
 ///MOve player ou(move tile set)
   if(moveR &&!collisionRight.collideBolean){
      player.x+=player.spd;
@@ -148,14 +190,16 @@ collisionDown.collide(player.x,player.y,player.w,player.h)
    // yTiles+=player.spd
   }
 
-  /*
+
+  
+  
   collisionUp.draw("red")
   collisionLeft.draw("blue")
   collisionRight.draw("green")
   collisionDown.draw("orange")
-*/
 
-  player.hudMsg(player.x,player.y,`${xTiles}`)
+
+ tileDungeon.hudMsg(tileDungeon.x,tileDungeon.y-64,`${xTiles}/${yTiles}/${tileId}`)
 
 
 };
